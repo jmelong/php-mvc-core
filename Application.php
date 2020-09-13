4f02f0520e4cd8d3101ee02c98946783c79eee03
Jamie Long <jamielong1221@gmail.com>
2020-09-13 23:16:59 +0100

    Add events on Application.php

--- Application.php
+++ Application.php
@@ -12,8 +12,12 @@ use app\models\User;
  */
 class Application
 {
-    public static string $ROOT_DIR;
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
 
+    protected array $eventListeners = [];
+
+    public static string $ROOT_DIR;
     public string $layout = 'main';
     public string $userClass = '';
     public Router $router;
@@ -54,6 +58,7 @@ class Application
     }
 
     public function run(){
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
         try{
             echo $this->router->resolve();
         }catch(\Exception $e){
@@ -62,6 +67,19 @@ class Application
                 'exception' => $e
             ]);
         }
+    }
+
+    public function triggerEvent($eventName)
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
+    }
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName][] = $callback;
     }
 
     /**
